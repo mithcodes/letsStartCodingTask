607694e1934cb4c468af3dff98015c6b0dfc3102
mithcodes <137909653+mithcodes@users.noreply.github.com>
2023-12-13 01:01:16 +0530

    first commit

---
+++ one/src/App.js
@@ -0,0 +1,12 @@
+import List from './components/list'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <List></List>
+    </>
+  );
+}
+
+export default App;
---
+++ one/src/components/list.js
@@ -0,0 +1,15 @@
+
+
+function List(){
+    return(
+    <>
+<h1>Expenses Item</h1>
+<h3>food ₹10</h3>
+<h3>petrol ₹10</h3>
+<h3>movie ₹ 200</h3>
+</>
+
+)
+    }
+
+export default List
